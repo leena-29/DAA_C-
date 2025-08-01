19662a5bb4f29ac4f8f3d8b2b02a2deaf34bd962
LEENA <131523797+leena-29@users.noreply.github.com>
2025-08-01 23:25:31 +0530

    Create drawing_book.cpp

---
+++ drawing_book.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int pageCount(int n, int p) {
+    int front = p / 2;
+    int back = (n / 2) - (p / 2);
+    return min(front, back);
+}
+
+int main() {
+    int n, p;
+    cin >> n >> p;
+    cout << pageCount(n, p);
+    return 0;
+}
