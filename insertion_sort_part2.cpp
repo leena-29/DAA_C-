3915e5e8f05767262420ee0f6f5e5b8a082beb7e
LEENA <131523797+leena-29@users.noreply.github.com>
2025-10-02 23:48:18 +0530

    Create insertion_sort_part2.cpp

---
+++ insertion_sort_part2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(vector<int> &arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void insertionSort2(int n, vector<int> &arr) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Shift elements greater than key
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+
+        // Print after each iteration
+        printArray(arr);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    insertionSort2(n, arr);
+
+    return 0;
+}
