e3e1e66fed4bae6190f2c1357a480488f50041ab
LEENA <131523797+leena-29@users.noreply.github.com>
2025-10-16 23:26:43 +0530

    Update funny_string.cpp

--- funny_string.cpp
+++ funny_string.cpp
@@ -1,46 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> weightedUniformStrings(string s, vector<int> queries) {
-    unordered_set<int> weights;
-    int currentWeight = 0;
-    char prevChar = '\0';
-
-    for (char c : s) {
-        int weight = c - 'a' + 1;
-        if (c == prevChar) {
-            currentWeight += weight;
-        } else {
-            currentWeight = weight;
-            prevChar = c;
-        }
-        weights.insert(currentWeight);
+string funnyString(string s) {
+    string rev = s;
+    reverse(rev.begin(), rev.end());
+    
+    for (int i = 1; i < s.size(); i++) {
+        int diff1 = abs(s[i] - s[i - 1]);
+        int diff2 = abs(rev[i] - rev[i - 1]);
+        if (diff1 != diff2)
+            return "Not Funny";
     }
-
-    vector<string> result;
-    for (int q : queries) {
-        if (weights.find(q) != weights.end())
-            result.push_back("Yes");
-        else
-            result.push_back("No");
-    }
-
-    return result;
+    return "Funny";
 }
 
 int main() {
-    string s;
-    cin >> s;
-
-    int n;
-    cin >> n;
-    vector<int> queries(n);
-    for (int i = 0; i < n; i++)
-        cin >> queries[i];
-
-    vector<string> result = weightedUniformStrings(s, queries);
-    for (string ans : result)
-        cout << ans << endl;
-
+    int q;
+    cin >> q;
+    while (q--) {
+        string s;
+        cin >> s;
+        cout << funnyString(s) << endl;
+    }
     return 0;
 }
