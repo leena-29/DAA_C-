d36be96508bf9158b2682294136210c3af3f0b84
LEENA <131523797+leena-29@users.noreply.github.com>
2025-11-26 11:37:55 +0530

    Update TheLoveLetterMystery.cpp

--- TheLoveLetterMystery.cpp
+++ TheLoveLetterMystery.cpp
@@ -1,61 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> closestNumbers(vector<int> arr) {
-    sort(arr.begin(), arr.end());
-
-    vector<int> result;
-    int minDiff = INT_MAX;
-
-    for (int i = 1; i < arr.size(); i++) {
-        int diff = arr[i] - arr[i - 1];
-        if (diff < minDiff) {
-            minDiff = diff;
-            result.clear();
-            result.push_back(arr[i - 1]);
-            result.push_back(arr[i]);
-        } else if (diff == minDiff) {
-            result.push_back(arr[i - 1]);
-            result.push_back(arr[i]);
-        }
-    }
-    return result;
-}
-
-int main() {
-    int n;
-    cin >> n;
-
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) cin >> arr[i];
-
-    vector<int> ans = closestNumbers(arr);
-    for (int x : ans) cout << x << " ";
-}
-#include <bits/stdc++.h>
-using namespace std;
-
 int theLoveLetterMystery(string s) {
-    int n = s.size();
+    int n = s.length();
     int operations = 0;
-    
-    // Compare characters from both ends toward the center
-    for(int i = 0; i < n / 2; i++) {
-        operations += abs(s[i] - s[n - i - 1]);
+
+    int l = 0, r = n - 1;
+    while (l < r) {
+        operations += abs(s[l] - s[r]);
+        l++;
+        r--;
     }
-    
     return operations;
 }
 
 int main() {
     int q;
     cin >> q;
-    
-    while(q--) {
+    while (q--) {
         string s;
         cin >> s;
         cout << theLoveLetterMystery(s) << endl;
     }
-    
     return 0;
 }
