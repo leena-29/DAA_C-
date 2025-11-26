2789002631d8c97a90adad733ca2a835f03ae6c7
LEENA <131523797+leena-29@users.noreply.github.com>
2025-11-26 11:35:51 +0530

    Update TheLoveLetterMystery.cpp

--- TheLoveLetterMystery.cpp
+++ TheLoveLetterMystery.cpp
@@ -1,6 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> closestNumbers(vector<int> arr) {
+    sort(arr.begin(), arr.end());
+
+    vector<int> result;
+    int minDiff = INT_MAX;
+
+    for (int i = 1; i < arr.size(); i++) {
+        int diff = arr[i] - arr[i - 1];
+        if (diff < minDiff) {
+            minDiff = diff;
+            result.clear();
+            result.push_back(arr[i - 1]);
+            result.push_back(arr[i]);
+        } else if (diff == minDiff) {
+            result.push_back(arr[i - 1]);
+            result.push_back(arr[i]);
+        }
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    vector<int> ans = closestNumbers(arr);
+    for (int x : ans) cout << x << " ";
+}
+#include <bits/stdc++.h>
+using namespace std;
+
 int theLoveLetterMystery(string s) {
     int n = s.size();
     int operations = 0;
